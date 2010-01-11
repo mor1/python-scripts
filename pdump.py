f3d173c8ded9b19590ca52a4ab5e27093a6283da
Richard Mortier <mort@cantab.net>
2010-01-11 22:48:41 +0000

    pdump.py: initial commit

---
+++ pdump.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+#
+# Simple hex raw packet dump, using SOCK_RAW (Linux)
+#
+# Copyright (C) 2002 Richard Mortier <mort@cantab.net>.  All Rights
+# Reserved.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+# USA.
+
+import sys, socket
+
+dev = 'eth0'
+
+s = socket.socket(socket.PF_PACKET, socket.SOCK_RAW, socket.htons(0x0003))
+sockaddr = (dev, 0x0000)
+s.bind(sockaddr)
+x = s.getsockname()
+print x
+
+while 1:
+    y = s.recv(1024)
+    print `y`
+    sys.stdout.flush()
