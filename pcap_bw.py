c358b06dc084c9a0ee0fd134a47dcbca7e38a7ad
Richard Mortier <mort@cantab.net>
2020-04-02 13:46:51 +0100

    pcap_bw: add GPL v3 script for extracting bandwidth from PCAP files
    Signed-off-by: Richard Mortier <mort@cantab.net>

---
+++ pcap_bw.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+#
+# Copyright (c) 2020 Richard Mortier <mort@cantab.net>
+#
+# Licensed under the GPL v3; see <./LICENSE.md> in the root of this
+# distribution or the full text at https://opensource.org/licenses/GPL-3.0
+#
+# Requires `pip|pip3 install dpkt`.
+#
+# Useful pre-processing command lines for large PCAP files include:
+#
+# $ editcap -S0 -d -A"YYYY-MM-DD HH:mm:SS" -B"YYYY-MM-DD HH:mm:SS" in.pcap fragment.pcap
+
+import sys, socket, pprint, json
+import dpkt
+
+## dpkt.pcap.Reader iterator doesn't provide the PCAP header, only the timestamp
+class R(dpkt.pcap.Reader):
+    def __iter__(self):
+        while 1:
+            buf = self._Reader__f.read(dpkt.pcap.PktHdr.__hdr_len__)
+            if not buf:
+                break
+            hdr = self._Reader__ph(buf)
+            buf = self._Reader__f.read(hdr.caplen)
+            yield (hdr.tv_sec + (hdr.tv_usec / self._divisor), hdr, buf)
+
+## from dpkt print_pcap example
+def inet_to_str(inet):
+    try:
+        return socket.inet_ntop(socket.AF_INET, inet)
+    except ValueError:
+        return socket.inet_ntop(socket.AF_INET6, inet)
+
+if __name__ == '__main__':
+    ## parameters; one day, these will be command line options. maybe.
+    INPUT  = "out.pcap"
+    WINDOW = 1 ## seconds
+    HOSTS  = [ '10.0.0.'+str(i+1) for i in range(4) ]
+
+    with open(INPUT, 'rb') as f:
+        pcap = R(f)
+        cnt = 0
+        prevts = 0
+        prevwindow = 0
+        totbw  = 0
+        hostbw = { i: { j: 0 for j in HOSTS } for i in HOSTS }
+        for ts, hdr, buf in pcap:
+            if prevts == 0:
+                s = ", ".join(
+                    ",".join([":".join([s,d]) for d in HOSTS])
+                    for s in HOSTS
+                )
+                print("# time, totalbw, %s" % s, sep=",", flush=True)
+            cnt += 1
+            if cnt % 10000 == 0:
+                print(cnt, "...", end="", sep="", flush=True, file=sys.stderr)
+
+            sll = dpkt.sll.SLL(buf) ## i happen to know the input linktype = SLL
+
+            pkt = None
+            if sll.ethtype == 0x0800: ## IPv4
+                if sll.type == 3: ## sent to someone else
+                    pkt = sll.ip
+                elif sll.type == 4: ## sent by us, ie., emitted from switch
+                    pass
+                else:
+                    print("[dropped %04x / %d]..." % (sll.ethtype, sll.type),
+                          end="", sep="", file=sys.stderr)
+            elif sll.ethtype == 0x0806: ## ARP
+                print("[dropped ARP / %d bytes]..." % hdr.len,
+                      end="", sep="", file=sys.stderr)
+            else:
+                print("[dropped %04x / %d]..." % (sll.ethtype, sll.type),
+                      end="", sep="", file=sys.stderr)
+
+            if not pkt: continue
+
+            window = int(ts) if WINDOW == 1 else WINDOW * (int(ts) // WINDOW)
+            if prevwindow == 0: prevwindow = window
+            if prevwindow != window:
+                hostbws = ", ".join(
+                    ",".join([ str(hostbw[s][d]) for d in HOSTS ])
+                     for s in HOSTS
+                )
+                print(prevwindow, totbw, hostbws, sep=", ", flush=True)
+                totbw = 0
+                hostbw = { i: { j: 0 for j in HOSTS } for i in HOSTS }
+                prevwindow = window
+
+            totbw += pkt.len
+            src = inet_to_str(pkt.src)
+            dst = inet_to_str(pkt.dst)
+            hostbw[src][dst] += pkt.len
+
+            prevts = ts
